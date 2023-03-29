9303e9144877ef93403e4b3987ea5c28677bdf5b
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-03-29 09:00:13 +0530

    [Rushi]: Create Product review class with ProductID, UserID, Rating, Review and isLike fields

---
+++ ProductReviewManagementUsingLINQ/ProductReviewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductReviewManagementUsingLINQ
+{
+    public class ProductReviewModel
+    {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
+    }
+}
---
+++ ProductReviewManagementUsingLINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace ProductReviewManagementUsingLINQ
+{
+    public class Program
+    {
+        // LinQ -  it is language integrated Query . it use for grouping , sorting and ordering data.
+        public static void Main(string[] args)
+        {
+            List<ProductReviewModel> listOfProductReview = new List<ProductReviewModel>()
+            {
+                new ProductReviewModel { ProductId = 1 , UserId = 11 , Rating = 5 , Review = "Good" , isLike = true  },
+                new ProductReviewModel { ProductId = 2 , UserId = 10 , Rating = 2 , Review = "Bad" , isLike = false  },
+                new ProductReviewModel { ProductId = 3 , UserId = 11 , Rating = 4 , Review = "Good" , isLike = true  },
+                new ProductReviewModel { ProductId = 4 , UserId = 11 , Rating = 1 , Review = "Good" , isLike = true  },
+                new ProductReviewModel { ProductId = 5 , UserId = 11 , Rating = 3 , Review = "Average" , isLike = true  },
+                new ProductReviewModel { ProductId = 6 , UserId = 10 , Rating = 5 , Review = "Good" , isLike = true  },
+                new ProductReviewModel { ProductId = 7 , UserId = 10 , Rating = 2 , Review = "Bad" , isLike = false  },
+                new ProductReviewModel { ProductId = 8 , UserId = 8 , Rating = 5 , Review = "Good" , isLike = true  },
+                new ProductReviewModel { ProductId = 9 , UserId = 11 , Rating = 1 , Review = "Bad" , isLike = false  },
+                new ProductReviewModel { ProductId = 10 , UserId = 9 , Rating = 5 , Review = "Good" , isLike = true  },
+                new ProductReviewModel { ProductId = 11 , UserId = 10 , Rating = 1 , Review = "Bad", isLike = false  },
+                new ProductReviewModel { ProductId = 12 , UserId = 10 , Rating = 1 , Review = "Bad", isLike = false  },
+                new ProductReviewModel { ProductId = 13 , UserId = 10 , Rating = 1 , Review = "Bad", isLike = false  },
+                new ProductReviewModel { ProductId = 11 , UserId = 7 , Rating = 1 , Review = "Bad", isLike = false  },
+                new ProductReviewModel { ProductId = 11 , UserId = 6 , Rating = 1 , Review = "Bad", isLike = false  },
+                new ProductReviewModel { ProductId = 11 , UserId = 5 , Rating = 1 , Review = "Bad", isLike = false  },
+            };
+
+        }
+    }
+}
